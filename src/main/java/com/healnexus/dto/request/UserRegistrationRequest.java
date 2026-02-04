393813c77c0a3f60d8750b1379423773fa46d707
MahammadAlfaz <alfazkota.786@gmail.com>
2026-02-04 17:38:02 +0530

    feat: add user registration request DTO

---
+++ src/main/java/com/healnexus/dto/request/UserRegistrationRequest.java
@@ -0,0 +1,22 @@
+package com.healnexus.dto.request;
+
+import com.healnexus.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequest {
+    @NotBlank
+    @Email(message = "Invalid email address ")
+    private String email;
+    @NotBlank
+    @Size(min = 8 ,message = "password must be at least 8 character")
+    private String password;
+    @NotNull
+    private Role role;
+}
